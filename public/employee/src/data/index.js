cff34a25deff5af30d17b6377a8fa83067a2258a
itcdnfs01 <itcdnfs01@ITCDNFS01>
2022-09-27 16:38:00 +0530

    first commit

---
+++ public/employee/src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Jeetu ',
+    lastName: 'kumar',
+    email: 'jeetu@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Vijit',
+    lastName: 'nagpure',
+    email: 'vijit@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Amrita',
+    lastName: 'Sarkar',
+    email: 'amrita@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Akhil',
+    lastName: 'tumala',
+    email: 'kent@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Kanthika',
+    lastName: 'sharma',
+    email: 'kanthika@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Sumit',
+    lastName: 'sonu',
+    email: 'sumit@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Anil',
+    lastName: 'Reddy',
+    email: 'anil@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'uvaana',
+    lastName: 'uvaana',
+    email: 'uvaan@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Rashmika',
+    lastName: 'sharma',
+    email: 'Rashmika@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Chandu',
+    lastName: 'priya',
+    email: 'chandu@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
